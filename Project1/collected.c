08349bed850139cd35b7be08db521232f1019607
Cat Hanbit <nocaca@naver.com>
2020-06-11 00:10:11 +0900

    chapter 4 04-02

--- Project1/collected.c
+++ Project1/collected.c
@@ -11,7 +11,21 @@ int main() {
 	printf("AVG : %.2f\n", (num1+(float)num2)/2);
 
 	return 0;
-}
-00:04:53.87
+}	00:04:53.87
+
+#include <stdio.h>
+#include <float.h>
+int main() {
+	int num1,nH, nM,nS;
+
+	scanf_s("%d", &num1);
+	nS = num1 % 60;
+	nM =  (num1 / 60)%60;
+	nH = num1 / 3600;
+
+	printf("%d초는 %02d시간 %02d분 %02d초 입니다.\n",  num1,nH,nM,nS);
+	return 0;
+}	00:09 : 10.58
+
 
  */
