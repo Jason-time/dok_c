26d9aebbe87637586d4520e036d81ee062fab666
Cat Hanbit <nocaca@naver.com>
2020-06-12 00:11:38 +0900

    p.197 chapter 6 branching

--- Project1/Helloworld.c
+++ Project1/Helloworld.c
@@ -5,14 +5,19 @@ int main(void) {
 	int nAge, nFee = 1000;
 	double dResult;
 
-	scanf_s("%d", &nAge); 
+	scanf_s("%d", &nAge);
 	dResult = nFee;
 
-	if (nAge < 20) { 
-		dResult = nFee * 0.75;
+	if (nAge <= 3) {
+		dResult = nFee * 0;
 	}
+	else if (nAge <= 13)		dResult = nFee * 0.5;
+	else if (nAge <= 19)		dResult = nFee * 0.75;
+	else 	dResult = nFee * 1.0;
 
-		printf("최종요금:%.f원", dResult);
-		return 0;
-}  // p.187	chpater 06-02 00:10:55.49
+	printf("최종요금: %.f원", dResult);
+	return 0;
+}  // p.197	chpater6 branching  00:07:22.35
+
+
 
