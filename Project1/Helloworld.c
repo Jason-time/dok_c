15f35759d1b6da27d3c0d7a6addbaff361581c82
Cat Hanbit <nocaca@naver.com>
2020-06-12 23:33:22 +0900

    chapter7 p.242 Print out pyrmid

--- Project1/Helloworld.c
+++ Project1/Helloworld.c
@@ -2,17 +2,19 @@
 
 int main(void) {
 
-	int  nStartNum = 1, nEndNum = 5, i, j;
+	int  nStartNum = 1, nEndNum = 5, i, j, k;
 
 	for (i = nStartNum; i <= nEndNum; i++) {
-		for (j = nEndNum-i; j >= nStartNum;j--) {
+		for (j = nEndNum - i; j >= nStartNum;j--) {
 			printf("\t");
 		}
-		for(j=nStartNum ; j<=i ; j++)			printf("\t*");
+		for (j = nStartNum; j <= i; j++)	printf("\t*");
+		for (k = nStartNum; k < i; k++) printf("\t*");
 		putchar('\n');
 	}
 
 	return 0;
-}  // p.241	chpater7 07-06 00:06:57.59
+}  // chpater7 p.241 07-07 Print out pyramid	00:05:33.97
+
 
 
