eca07e6e5243df0bed056e3d4ac97568f266f098
Cat Hanbit <nocaca@naver.com>
2020-06-12 19:21:52 +0900

    p.241 chapter7 07-06

--- Project1/Helloworld.c
+++ Project1/Helloworld.c
@@ -5,12 +5,14 @@ int main(void) {
 	int  nStartNum = 1, nEndNum = 5, i, j;
 
 	for (i = nStartNum; i <= nEndNum; i++) {
-		for (j = nStartNum; j <= i;j++) {
-			printf("* ");
+		for (j = nEndNum-i; j >= nStartNum;j--) {
+			printf("\t");
 		}
+		for(j=nStartNum ; j<=i ; j++)			printf("\t*");
 		putchar('\n');
 	}
 
 	return 0;
-}  // p.241	chpater7 07-05 00:10:50.19
+}  // p.241	chpater7 07-06 00:06:57.59
+
 
