4cb4c82ca97ef5bb3144586db552dcf4d387b978
Cat Hanbit <nocaca@naver.com>
2020-06-12 17:42:30 +0900

    p.237 07-03 Sum 1to10

--- Project1/Helloworld.c
+++ Project1/Helloworld.c
@@ -2,20 +2,12 @@
 
 int main(void) {
 
-	int nRoop1 = 0, nRoop2 = 0, nCnt = 5;
+	int nSum = 0, nStartNum = 1,nEndNum=10;
 
-	while (nRoop1 < nCnt) {
-		nRoop2 = 0;
-		while (nRoop2 < nCnt) {
-			putchar('*');
-			putchar('\t');
-			nRoop2++;
-		}
-		putchar('\n');
-		nRoop1++;
-	}  
+	while (nStartNum <= nEndNum) {
+		nSum += nStartNum;
+		nStartNum++;
+	} 
+	printf("%d",nSum);
 	return 0;
-}  // p.234	chpater7 5 by 5 nested roop    ??
-
-
-
+}  // p.237	chpater7 07-03 add 1 to 10	00:06:21.30
--- Project1/collected.c
+++ Project1/collected.c
@@ -192,4 +192,19 @@ int main(void) {
 	return 0;
 }  // p.234	chpater7 5 by 5 nested roop    ??
 
+#include <stdio.h>
+
+int main(void) {
+
+	int nSum = 0, nStartNum = 1,nEndNum=10;
+
+	while (nStartNum <= nEndNum) {
+		nSum += nStartNum;
+		nStartNum++;
+	}
+	printf("%d",nSum);
+	return 0;
+}  // p.237	chpater7 07-03 add 1 to 10	00:06:21.30
+
+
  */
