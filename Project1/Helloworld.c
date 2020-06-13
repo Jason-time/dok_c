a3ebcf5ee6e3b7eeee32354ca7475f214f403043
Jason-time <noconal+jason-time-github@gmail.com>
2020-06-14 04:01:09 +0900

    chpater09 09-03 p.306 selection sort

--- Project1/Helloworld.c
+++ Project1/Helloworld.c
@@ -4,16 +4,18 @@ int main(void) {
 
 	int aList[5] = { 30,40,10,50,20 };
 
-	int i = 0, j = 0, nTmp = 0;
-	// 
-	for (i=5-1;i > 0;--i) { 
+	int i = 0, j = 0, mMinEndex = 0, nTmp = 0;
 
-		for (j = 0;j < i;++j) {
-			if (aList[j] > aList[j+1]) {
-				nTmp = aList[j];
-				aList[j] = aList[j+1];
-				aList[j+1] = nTmp;
+	// 
+	for (i=0;i < 4;++i) { 
+		mMinEndex = i;
+		for (j = i+1;j < 5;++j) {
+			if (aList[mMinEndex] > aList[j]) {
+				mMinEndex = j;
 			} 
+				nTmp = aList[mMinEndex];
+				aList[mMinEndex] = aList[i];
+				aList[i] = nTmp;
 		} 
 	}
 	
@@ -24,5 +26,6 @@ int main(void) {
 	putchar('\n');
 
 	return 0;
-}  // chpater09 09-02 p.303  Bubble sort2	00:42:32.89
+}  // chpater09 09-03 p.306 selection sort	00:19:25.78
+
 
--- Project1/collected.c
+++ Project1/collected.c
@@ -426,7 +426,35 @@ int main(void) {
 	return 0;
 }  // chpater09 09-02 p.303  Bubble sort2	00:42:32.89
 
+#include <stdio.h>
 
+int main(void) {
+
+	int aList[5] = { 30,40,10,50,20 };
+
+	int i = 0, j = 0, mMinEndex = 0, nTmp = 0;
+
+	//
+	for (i=0;i < 4;++i) {
+		mMinEndex = i;
+		for (j = i+1;j < 5;++j) {
+			if (aList[mMinEndex] > aList[j]) {
+				mMinEndex = j;
+			}
+				nTmp = aList[mMinEndex];
+				aList[mMinEndex] = aList[i];
+				aList[i] = nTmp;
+		}
+	}
+
+	//
+	for (i = 0;i < 5;++i) {
+		printf("%d\t", aList[i]);
+	}
+	putchar('\n');
+
+	return 0;
+}  // chpater09 09-03 p.306 selection sort	00:19:25.78
 
 
 */
