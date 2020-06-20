c8841f76210c6e0ad2855605df4f85c32cc4ca1d
Jason-time <noconal+jason-time-github@gmail.com>
2020-06-21 05:42:34 +0900

    chpater09 p.315 arraylookup01.c

--- Project1/Helloworld.c
+++ Project1/Helloworld.c
@@ -1,31 +1,29 @@
 #include <stdio.h>
 
-int main(void) {
+int main(int argc, char* argv[]) {
+	//전체 과금률 배열
+	double aRate[10] = {
+		0.0,0.1,0.25, //1~3세
+		0.5,0.5,		//4,5세
+		0.6,0.65,		//6,7세
+		0.8,0.82,0.97	//8~10세
+	};
+	int nAge = 0, i = 0, nFee = 1000;
 
-	int aList[5][5] = { 0 };
+	//요금표 출력
+	printf("\t요금표\n\n");
+	for (i = 1;i <= 10;++i)
+		printf("%2d세 요금 : \t%d원\n", i, (int)(nFee * aRate[i - 1]));;
+	putchar('\n');
 
-	int x = -1, y = 0, nCounter = 0;
-	int i = 0, j = 0, nLength = 9, nDirection = 1;
+	//나이 입력 후 인덱스 적용
+	printf("나이를 입력하세요. : ");
+	scanf_s("%d", &nAge);
+	if (nAge < 1)		nAge = 1;
+	else if (nAge > 10)	nAge = 10;
 
-	for (nLength = 9; nLength > 0; nLength -= 2) {
-		for (i = 0; i < nLength; ++i) {
-
-			if (i < nLength / 2 + 1)	x += nDirection;
-			else						y += nDirection;
-
-			aList[y][x] = ++nCounter;
-		}
-		nDirection = -nDirection;
-	}
-
-	
-
-	// 배열 출력
-	for (i = 0;i < 5;++i) {
-		for (j = 0;j < 5;++j)
-			printf("%d\t", aList[i][j]);
-		putchar('\n');
-	}
-
+	//나이에 맞는 배열 선택 후 출력
+	printf("최종요금 : %d원\n", (int)(nFee * aRate[nAge - 1]));
+	putchar('\n');
 	return 0;
-}  // chpater09 p.314 09-04  snail01.c	
+}  // chpater09 p.315 arraylookup01.c
--- Project1/collected.c
+++ Project1/collected.c
@@ -586,5 +586,35 @@ int main(void) {
 	return 0;
 }  // chpater09 p.314 09-04  snail01.c
 
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+	//전체 과금률 배열
+	double aRate[10] = {
+		0.0,0.1,0.25, //1~3세
+		0.5,0.5,		//4,5세
+		0.6,0.65,		//6,7세
+		0.8,0.82,0.97	//8~10세
+	};
+	int nAge = 0, i = 0, nFee = 1000;
+
+	//요금표 출력
+	printf("\t요금표\n\n");
+	for (i = 1;i <= 10;++i)
+		printf("%2d세 요금 : \t%d원\n", i, (int)(nFee * aRate[i - 1]));;
+	putchar('\n');
+
+	//나이 입력 후 인덱스 적용
+	printf("나이를 입력하세요. : ");
+	scanf_s("%d", &nAge);
+	if (nAge < 1)		nAge = 1;
+	else if (nAge > 10)	nAge = 10;
+
+	//나이에 맞는 배열 선택 후 출력
+	printf("최종요금 : %d원\n", (int)(nFee * aRate[nAge - 1]));
+	putchar('\n');
+	return 0;
+}  // chpater09 p.315 arraylookup01.c
+
 
 */
