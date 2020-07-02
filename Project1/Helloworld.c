799f80ba8743b409bebfee3a111257d9f46c5f29
Jason-time <noconal+jason-time-github@gmail.com>
2020-07-02 16:56:41 +0900

    chpater10 p.344 PrintMenu02.c

--- Project1/Helloworld.c
+++ Project1/Helloworld.c
@@ -2,49 +2,39 @@
 #include<conio.h>
 #include<stdlib.h>
 
+int GetFee(int nAge) {
+	int nFee = 1000;
+	if (nAge < 20) nFee /= 2;
+	return nFee;
+}
+
+
 int PrintMenu(void) {
 	int nInput = 0;
 	system("cls");
 	printf("[1]New\t[2]Search\t[3]Print\t[4]Remove\t[0]Exit\n:");
 	scanf_s("%d", &nInput);
 	return nInput;
+}
 
+int GetAge(void) {
+	int nAge = 0;
+	printf("나이를 입력하세요. : ");
+	scanf_s("%d", &nAge);
+	return nAge;
 }
 
 int main(void) {
-	int nMenu = 0;
 
-	while ((nMenu = PrintMenu()) != 0) {
-		switch (nMenu)
-		{
-		case 1:
-			puts("새 항목을 추가합니다.");
+	int nMenu = 0, nAge = 0;
+	
+	while ((nMenu = PrintMenu()) != 0){
+		if(nMenu==1){
+			nAge = GetAge();
+			printf("요금은 %d원 입니다. \n", GetFee(nAge));
 			_getch();
-
-			break;
-
-		case 2:
-			puts("기존 항목에서 검색합니다.");
-			_getch();
-			break;
-
-		case 3:
-			puts("전체 내용을 출력합니다.");
-			_getch();
-			break;
-
-		case 4:
-			puts("기존 항목을 삭제합니다.");
-			_getch();
-			break;
-
-		default:
-			puts("알 수 없는 명령입니다.");
-			_getch();
-			break;
 		}
-		_getch;
 	}
 	puts("Bye~~!");
 	return 0;
-}  // chpater10 p.342 Printmenu01.c
+}  // chpater10 p.344 PrintMenu02.c
--- Project1/collected.c
+++ Project1/collected.c
@@ -794,5 +794,47 @@ int main(void) {
 	}
 	puts("Bye~~!");
 	return 0;
-}  // chpater10 p.342 Printmenu01.c
+}  // chpater10 p.342 PrintMenu01.c
+
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+int GetFee(int nAge) {
+	int nFee = 1000;
+	if (nAge < 20) nFee /= 2;
+	return nFee;
+}
+
+
+int PrintMenu(void) {
+	int nInput = 0;
+	system("cls");
+	printf("[1]New\t[2]Search\t[3]Print\t[4]Remove\t[0]Exit\n:");
+	scanf_s("%d", &nInput);
+	return nInput;
+}
+
+int GetAge(void) {
+	int nAge = 0;
+	printf("나이를 입력하세요. : ");
+	scanf_s("%d", &nAge);
+	return nAge;
+}
+
+int main(void) {
+
+	int nMenu = 0, nAge = 0;
+
+	while ((nMenu = PrintMenu()) != 0){
+		if(nMenu==1){
+			nAge = GetAge();
+			printf("요금은 %d원 입니다. \n", GetFee(nAge));
+			_getch();
+		}
+	}
+	puts("Bye~~!");
+	return 0;
+}  // chpater10 p.344 PrintMenu02.c
+
 */
